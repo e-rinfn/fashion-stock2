47fb307a84174637ee16163685c9206a818c5fd1
Erin Fajrin Nugraha <erinfajinnugraha@gmail.com>
2025-06-06 21:56:50 +0700

    perbaikan di halmaan login

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once '../config/database.php';
-require_once '../config/functions.php';
+require_once '<?= $base_url ?>/config/database.php';
+require_once '<?= $base_url ?>/config/functions.php';
 
 if (isset($_POST['login'])) {
   $username = $conn->real_escape_string($_POST['username']);
@@ -19,9 +19,9 @@ if (isset($_POST['login'])) {
       $_SESSION['nama'] = $user['nama_lengkap'];
 
       if ($user['role'] == 'admin') {
-        header("Location: ../admin/index.php");
+        header("Location: <?= $base_url ?>/admin/index.php");
       } else {
-        header("Location: ../owner/index.php");
+        header("Location: <?= $base_url ?>/owner/index.php");
       }
       exit();
     }
@@ -37,7 +37,7 @@ if (isset($_POST['login'])) {
   class="light-style customizer-hide"
   dir="ltr"
   data-theme="theme-default"
-  data-assets-path="../assets/"
+  data-assets-path="<?= $base_url ?>/assets/"
   data-template="vertical-menu-template-free">
 
 <head>
@@ -51,7 +51,7 @@ if (isset($_POST['login'])) {
   <meta name="description" content="" />
 
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="<?= $base_url ?>/assets/img/favicon/favicon.ico" />
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -61,25 +61,25 @@ if (isset($_POST['login'])) {
     rel="stylesheet" />
 
   <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/vendor/fonts/boxicons.css" />
 
   <!-- Core CSS -->
-  <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="../assets/css/demo.css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/css/demo.css" />
 
   <!-- Vendors CSS -->
-  <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
   <!-- Page CSS -->
   <!-- Page -->
-  <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css" />
+  <link rel="stylesheet" href="<?= $base_url ?>/assets/vendor/css/pages/page-auth.css" />
   <!-- Helpers -->
-  <script src="../assets/vendor/js/helpers.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/js/helpers.js"></script>
 
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="../assets/js/config.js"></script>
+  <script src="<?= $base_url ?>/assets/js/config.js"></script>
 </head>
 
 <body>
@@ -92,7 +92,7 @@ if (isset($_POST['login'])) {
         <div class="card">
           <div class="card-body">
             <!-- Logo -->
-            <img src="../assets/img/Logo.png" width="100px" alt="Bakery Logo" class="mx-auto d-block mb-3">
+            <img src="<?= $base_url ?>/assets/img/Logo.png" width="100px" alt="Bakery Logo" class="mx-auto d-block mb-3">
             <div class="text-center">
               <div class="app-brand-link d-block">
                 <span class="text-body fw-bolder fs-3">Inventaris Barang <br> FASHION STOCK</span>
@@ -157,18 +157,18 @@ if (isset($_POST['login'])) {
 
   <!-- Core JS -->
   <!-- build:js assets/vendor/js/core.js -->
-  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-  <script src="../assets/vendor/libs/popper/popper.js"></script>
-  <script src="../assets/vendor/js/bootstrap.js"></script>
-  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/libs/popper/popper.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/js/bootstrap.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-  <script src="../assets/vendor/js/menu.js"></script>
+  <script src="<?= $base_url ?>/assets/vendor/js/menu.js"></script>
   <!-- endbuild -->
 
   <!-- Vendors JS -->
 
   <!-- Main JS -->
-  <script src="../assets/js/main.js"></script>
+  <script src="<?= $base_url ?>/assets/js/main.js"></script>
 
   <!-- Page JS -->
 
