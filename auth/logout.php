5de8bd2f05ab4e7014690d2cd930fba1f730e2c8
Erin Fajrin Nugraha <erinfajinnugraha@gmail.com>
2025-06-06 22:45:44 +0700

    perbaikan di login menjadi lebih sederhana

--- auth/logout.php
+++ auth/logout.php
@@ -5,5 +5,5 @@ include_once '../config/config.php';
 session_destroy();
 
 // Gunakan interpolasi variabel PHP secara benar
-header("Location: {$base_url}auth/login.php");
+header("Location: {$base_url}login.php");
 exit();
